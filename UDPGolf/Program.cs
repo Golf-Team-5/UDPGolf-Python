af0621dd64ff36db82936ab2d2834b3be3fcc13c
Rasmus Drotved Laursen <rasm781y@edu.easj.dk>
2019-12-02 11:09:44 +0100

    added connection to RestService

--- UDPGolf/Program.cs
+++ UDPGolf/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace UDPGolf
 {
     class Program
     {
+        static string ServerURI = "http://localhost:52549/api/swingdata/";
         static void Main(string[] args)
         {
             //opsætning af UDP socket
@@ -14,7 +18,6 @@ namespace UDPGolf
             UdpClient udpGolfServer = new UdpClient(6789);
             IPAddress ip = IPAddress.Parse("192.168.104.124");
             IPEndPoint golfIpEndPoint = new IPEndPoint(ip, 6789);
-
             
             try
             { Console.WriteLine("Server is waiting for input");
@@ -28,6 +31,7 @@ namespace UDPGolf
 
 
                     //TODO connect with RestRNGolfService and post to static list
+                    PostSwingData(new SwingData(Convert.ToDouble(receivedSwingData)));
 
                 }
             }
@@ -37,5 +41,28 @@ namespace UDPGolf
                 Console.WriteLine(e);
             }
         }
+
+        public static async Task<string> PostSwingData(SwingData swingData)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var jsonString = JsonConvert.SerializeObject(swingData);
+                    StringContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PostAsync(ServerURI, content);
+                    string str = await response.Content.ReadAsStringAsync();
+                    return str;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    
+                }
+
+                return null;
+            }
+        }
+
     }
 }
